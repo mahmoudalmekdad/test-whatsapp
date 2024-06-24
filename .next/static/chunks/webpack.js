b6d82758654ed3a696473712ddbb669f8a057d51
Mahmoud Almekdad <mahmuod@leadvy.com>
2024-06-25 00:34:51 +0400

    asfd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ceec473470686f25"; }
+/******/ 		__webpack_require__.h = function() { return "e8b1cdfafed273a8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

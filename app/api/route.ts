bd11f47ef84925ec2e99ca39aa7065588ec4f10c
Mahmoud Almekdad <mahmuod@leadvy.com>
2024-06-24 22:57:03 +0400

    push

---
+++ app/api/route.ts
@@ -0,0 +1,52 @@
+
+import { NextResponse } from "next/server";
+import axios from 'axios'
+// To handle a GET request to /api
+export async function GET(request:Request) {
+  // Do whatever you want
+  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+}
+
+// To handle a POST request to /api
+export async function POST(req: Request) {
+  // Do whatever you want
+  let body_param: any = req.body
+  console.log(JSON.stringify(body_param,null,2))
+
+  if(body_param && body_param.object){
+      if(body_param.entry && 
+          body_param.entry[0].changes &&
+          body_param.entry[0].changes[0].value.messages &&
+          body_param.entry[0].changes[0].value.messages[0]){
+
+          let phone_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id
+          let from = body_param.entry[0].changes[0].value.messages[0].from
+          let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body
+
+          axios({
+              method : "POST",
+              url : `https://graph.facebook.com/v19.0/${phone_no_id}/messages?access_token=${`EAANrLF1TIn8BO5vaM4YohWbYNqlRg72pZBJ8eBM3KXdFQDuNMDUc8ZCgEWgrJPaEpNgIQnsJOZCOG8B3Y6NBCWjfEKhPYwkeiVVWJKsLY51LhBkIy6O5FR0ZAec4WyBFHRZC0Vci1CKWjmBzqFLHQJXlXNXAB9JlHyb0hkNxfEnAQXyvWHIrLlPvfTzak5lcSrvINZCuvDvYFwZBZCjZB`}`,
+              data : {
+                  messaging_product : "whatsapp",
+                  to : from,
+                  text : {
+                      body:"Hi.. I'm Dr. Clinica, your message is "+msg_body
+                  }
+              },
+              headers : {
+                  "Content-Type" : "application/json"
+              }
+          }).then(function (response) {
+              console.log(response)
+          }).catch(function (error) {
+              console.log(error.toJSON());
+          });
+          return NextResponse.json({ message: "res: status(200)" }, { status: 200 });
+      }else{
+        return NextResponse.json({ message: "res: (400) " }, { status: 200 });
+    }
+  }else{
+    return NextResponse.json({ message: "res: (200) " }, { status: 200 });
+
+  }
+}
